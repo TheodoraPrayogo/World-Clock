677eb66a78518fd6f0098bcdb597a552ec3f79c2
TheodoraPrayogo <113406918+TheodoraPrayogo@users.noreply.github.com>
2023-03-21 22:05:16 -0500

    Added the select function

--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
-function updateJakartaTime () {
-//Jakarta
-let jakartaElement = document.querySelector("#jakarta");
-let jakartaDateElement = jakartaElement.querySelector(".date");
-let jakartaTimeElement = jakartaElement.querySelector(".time");
-let jakartaTime=moment().tz("Asia/Jakarta");
+function updateJakartaTime() {
+  //Jakarta
+  let jakartaElement = document.querySelector("#jakarta");
+  let jakartaDateElement = jakartaElement.querySelector(".date");
+  let jakartaTimeElement = jakartaElement.querySelector(".time");
+  let jakartaTime = moment().tz("Asia/Jakarta");
 
-jakartaDateElement.innerHTML=jakartaTime.format("MMMM Do, YYYY");
-jakartaTimeElement.innerHTML=jakartaTime.format("h:mm:ss [<small>]A[<small>]");
+  jakartaDateElement.innerHTML = jakartaTime.format("MMMM Do, YYYY");
+  jakartaTimeElement.innerHTML = jakartaTime.format(
+    "h:mm:ss [<small>]A[<small>]"
+  );
 }
 
-setInterval(updateJakartaTime,1000);
+setInterval(updateJakartaTime, 1000);
 
+function updateSydneyTime() {
+  //Sydney
 
-function updateSydneyTime (){
-//Sydney
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
 
-let sydneyElement = document.querySelector("#sydney");
-let sydneyDateElement = sydneyElement.querySelector(".date");
-let sydneyTimeElement = sydneyElement.querySelector(".time");
-let sydneyTime = moment().tz("Australia/Sydney");
-
-sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do, YYYY");
-sydneyTimeElement.innerHTML = sydneyTime.format(
-  "h:mm:ss [<small>]A[<small>]"
-);
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do, YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss [<small>]A[<small>]"
+  );
 }
 
-setInterval(updateSydneyTime,1000);
+setInterval(updateSydneyTime, 1000);
 
 function updateHoustonTime() {
   //Houston
@@ -44,3 +45,24 @@ function updateHoustonTime() {
 
 setInterval(updateHoustonTime, 1000);
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("US/Pacific", "Houston").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+      <div class="city">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
+    "A"
+  )}</small>
+        </div>
+      </div>
+  `;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
